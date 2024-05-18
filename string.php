ac0d743e1f13c8694455850a2074b2da3573147d
Margubur Rahman Mamshad <114665644+mrmamshad@users.noreply.github.com>
2024-05-18 21:10:45 +0600

    Add files via upload

---
+++ string.php
@@ -0,0 +1,26 @@
+<?php
+
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+
+function countVowels($str) {
+    $vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    $count = 0;
+    for ($i = 0; $i < strlen($str); $i++) {
+        if (in_array($str[$i], $vowels)) {
+            $count++;
+        }
+    }
+    return $count;
+}
+
+
+foreach ($strings as $string) {
+    $originalString = $string;
+    $vowelCount = countVowels($string);
+    $reversedString = strrev($string);
+    
+ 
+    echo "Original String: $originalString, Vowel Count: $vowelCount, Reversed String: $reversedString\n";
+}
+?>
